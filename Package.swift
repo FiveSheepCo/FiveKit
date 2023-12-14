a536ea7585f5da12c5a3c7d9000e3d2ab933a385
SplittyDev <splittydev@protonmail.com>
2023-12-14 18:50:15 +0100

    Reexport FoundationPlus and SwiftUIElements

--- Package.swift
+++ Package.swift
@@ -5,19 +5,31 @@ import PackageDescription
 
 let package = Package(
     name: "FiveKit",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macOS(.v10_15),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FiveKit",
             targets: ["FiveKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/FiveSheepCo/FoundationPlus.git", branch: "main"),
+        .package(url: "https://github.com/FiveSheepCo/SwiftUIElements.git", branch: "main"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FiveKit"),
-        .testTarget(
-            name: "FiveKitTests",
-            dependencies: ["FiveKit"]),
+            name: "FiveKit",
+            dependencies: [
+                "FoundationPlus",
+                "SwiftUIElements",
+            ]
+        )
     ]
 )
---
+++ Sources/FiveKit.swift
@@ -0,0 +1,4 @@
+/// FiveKit reexports all modules of FiveSheep's in-house tooling.
+
+@_exported import FoundationPlus
+@_exported import SwiftUIElements
