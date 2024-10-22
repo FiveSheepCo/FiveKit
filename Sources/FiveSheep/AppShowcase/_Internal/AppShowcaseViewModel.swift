0a1ac46255ee41e3de79dcb965b1d1eb3b9b8115
Jann Schafranek <me@jann.co>
2024-10-22 09:14:02 +0200

    Add cache refresh

--- Sources/FiveSheep/AppShowcase/_Internal/AppShowcaseViewModel.swift
+++ Sources/FiveSheep/AppShowcase/_Internal/AppShowcaseViewModel.swift
@@ -33,26 +33,38 @@ class AppShowcaseViewModel {
         
         let data: Data
         
-        if let storedData = UserDefaults.standard.data(forKey: Constants.userDefaultsStorageKey) {
+        if let storedData = UserDefaults.standard.data(forKey: Constants.userDefaultsStorageKey), let date = UserDefaults.standard.object(forKey: Constants.userDefaultsDateStorageKey) as? Date {
             data = storedData
+            
+            if date.timeIntervalUntilNow > 30 * 86400 {
+                Task {
+                    try await refreshData()
+                }
+            }
         } else {
-            
-            // Build request
-            var request = URLRequest(url: Constants.lookupUrl)
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            request.addValue(Constants.baseBundleId, forHTTPHeaderField: "X-Bundle-Id")
-            request.addValue(Constants.countryCode, forHTTPHeaderField: "X-Country-Code")
-            
-            // Await response
-            (data, _) = try await URLSession.shared.data(for: request)
-            
-            UserDefaults.standard.set(data, forKey: Constants.userDefaultsStorageKey)
+            data = try await refreshData()
         }
         
         // Decode response
         self.apps = try JSONDecoder().decode([AppDefinition].self, from: data).sorted(by: { a, b in
             a.name.compare(b.name) == .orderedAscending
         })
+    }
+    
+    private func refreshData() async throws -> Data {
+        // Build request
+        var request = URLRequest(url: Constants.lookupUrl)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue(Constants.baseBundleId, forHTTPHeaderField: "X-Bundle-Id")
+        request.addValue(Constants.countryCode, forHTTPHeaderField: "X-Country-Code")
+        
+        // Await response
+        let (data, _) = try await URLSession.shared.data(for: request)
+        
+        UserDefaults.standard.set(data, forKey: Constants.userDefaultsStorageKey)
+        UserDefaults.standard.set(Date.now, forKey: Constants.userDefaultsStorageKey)
+        
+        return data
     }
 }
 
@@ -62,6 +74,7 @@ extension AppShowcaseViewModel {
         static let lookupUrl = URL(string: "https://fivesheep.co/api/promotions.json")!
         static let countryCode = (Locale.current as NSLocale).countryCode ?? "US"
         static let userDefaultsStorageKey = "LoadedAppShowcase"
+        static let userDefaultsDateStorageKey = "LoadedAppShowcase-Date"
         
         static var baseBundleId: String {
             Bundle.main.bundleIdentifier!
